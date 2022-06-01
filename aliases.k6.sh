bb42e8085fe23efccc1ba68818b487f772275160
İnanç Gümüş <m@inanc.io>
2022-06-01 14:00:16 +0300

    Add k6b SKIP_FLAKY and linter

--- aliases.k6.sh
+++ aliases.k6.sh
@@ -1,21 +1,19 @@
-# TODO: make these funcs
-
 # fast tests
-alias kbt='go test ./common ./chromium -failfast'
+alias kbt='SKIP_FLAKY=true go test ./common ./chromium -failfast'
 
 # slower tests
-alias kbtf='go test ./tests -failfast -short'
+alias kbtf='SKIP_FLAKY=true go test ./tests -failfast -short'
 alias kbtfd='XK6_BROWSER_LOG=debug kbtf -v'
 alias kbtft='XK6_BROWSER_LOG=trace kbtf -v'
 
 # all tests
-alias kbta='go test ./common ./chromium ./tests -failfast -race'
+alias kbta='SKIP_FLAKY=true go test ./common ./chromium ./tests -failfast -race'
 alias kbtad="XK6_BROWSER_LOG=debug kbta -v"
 alias kbtat="XK6_BROWSER_LOG=trace kbta -v"
 
 # run scripts
-alias kbr='xk6 run -q'
+alias kbr="xk6 run --with github.com/grafana/xk6-browser=."
 alias kbrd="XK6_BROWSER_LOG=debug kbr"
 alias kbrt="XK6_BROWSER_LOG=trace kbr"
 
-alias kblint='golangci-lint run'
+alias kblint='BASEREV=$(git log main --pretty=format:'%h' -n 1) golangci-lint run --out-format=tab --new-from-rev "$BASEREV" ./...'
