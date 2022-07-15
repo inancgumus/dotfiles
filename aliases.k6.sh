375855da06595920dfe94412d28780e76598ee26
İnanç Gümüş <m@inanc.io>
2022-07-15 13:44:07 +0300

    Add kbdocs and modify some

--- aliases.k6.sh
+++ aliases.k6.sh
@@ -12,8 +12,12 @@ alias kbtad="XK6_BROWSER_LOG=debug kbta -v"
 alias kbtat="XK6_BROWSER_LOG=trace kbta -v"
 
 # run scripts
-alias kbr="xk6 run --with github.com/grafana/xk6-browser=."
+alias kbr="xk6 run -q"
 alias kbrd="XK6_BROWSER_LOG=debug kbr"
 alias kbrt="XK6_BROWSER_LOG=trace kbr"
 
-alias kblint='BASEREV=$(git log main --pretty=format:'%h' -n 1) golangci-lint run --out-format=tab --new-from-rev "$BASEREV" ./...'
+alias kblint='golangci-lint run --out-format=tab --new-from-rev "$(git log main --pretty=format:'%h' -n 1)" ./...'
+
+# docs
+alias kbdocsclean="rm -rf .node_modules && npm cache clean -f && npm i"
+alias kbdocsrun="npm run"
