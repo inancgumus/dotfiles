ca3b097411efad8a25988dc89bf8fce441426877
İnanç Gümüş <m@inanc.io>
2025-04-21 12:10:47 -0400

    Add SSH keys

--- install/ssh-keys.sh
+++ install/ssh-keys.sh
@@ -11,4 +11,5 @@ info "ensuring the keys have proper permissions\c"
 runCmd chmod -R 400 "$home/.ssh/*"
 
 info "adding your key to the os x key chain\c"
-runCmd ssh-add -K "$home/.ssh/id_rsa_minancio"
+runCmd ssh-add --apple-use-keychain "$home/.ssh/id_rsa_minancio"
+runCmd ssh-add --apple-use-keychain "$home/.ssh/id_rsa_grafana"
